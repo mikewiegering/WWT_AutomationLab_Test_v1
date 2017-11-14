0403d6fb2f48c2f7b1289a5cc6bd37bd0a8f3029
mikewiegering <mike.wiegering@gmail.com>
2017-11-14 16:00:51 -0600

    adding file

---
+++ PythonTest_v2.py
@@ -0,0 +1,10 @@
+print("How many tacos do you want?")
+
+tacos = input()
+
+
+def sentence(name='Bucky', action='ate', item='letils'):
+    print(name, action, item)
+
+print("You want " + tacos + " tacos")
+print('I like tacos')
